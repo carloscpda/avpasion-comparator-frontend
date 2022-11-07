9441fdbaa864d2efc089f08212afbe1b825b8f0a
Carlos Cepeda <carloscgarc@inditex.com>
2022-11-07 16:07:40 +0100

    implments clear cache with cors

--- pages/api/cache/reset.ts
+++ pages/api/cache/reset.ts
@@ -1,7 +1,36 @@
-import { NextApiHandler } from "next";
+import Cors from "cors";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import apollo from "../../../apollo-client";
 
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  origin: /avpasion\.com$/,
+  methods: ["POST"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
 const handler: NextApiHandler = async (req, res) => {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
   if (req.method === "POST") {
     await apollo.clearStore();
     res.status(200).json("Cache cleared");
