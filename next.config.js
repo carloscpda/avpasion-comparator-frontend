b06ae6ce2fa4e2f25d9aeef337ae8191847104e0
Carlos Cepeda <carlos@cepeda.me>
2022-11-12 14:03:00 +0100

    revert cdn

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd ? "https://cdn.avpasion.com" : undefined,
+  // assetPrefix: isProd ? 'https://cdn.avpasion.com' : undefined,
   images: {
     unoptimized: true,
     remotePatterns: [
