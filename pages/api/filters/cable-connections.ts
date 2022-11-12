ebb41d8fbec36a11d59427cc32cc1b68fb888541
Carlos Cepeda <carlos@cepeda.me>
2022-11-12 17:00:36 +0100

    add redis

--- pages/api/filters/cable-connections.ts
+++ pages/api/filters/cable-connections.ts
@@ -1,10 +1,26 @@
+import Redis from "ioredis";
 import { NextApiHandler } from "next";
 import getCableConnectionsFilter from "../../../graphql/get-cable-connections-filter";
 
 const handler: NextApiHandler = async (req, res) => {
   if (req.method === "GET") {
-    const cableConnections = await getCableConnectionsFilter();
-    res.setHeader("Cache-Control", "s-maxage=600");
+    const redis = new Redis();
+
+    let cableConnectionsCached = await redis.get("cable-connections");
+    let cableConnections = null;
+
+    if (!cableConnectionsCached) {
+      cableConnections = await getCableConnectionsFilter();
+      redis.set(
+        "cable-connections",
+        JSON.stringify(cableConnections),
+        "EX",
+        86400
+      );
+    } else {
+      cableConnections = JSON.parse(cableConnectionsCached);
+    }
+
     res.status(200).json(cableConnections);
   } else {
     res.status(404).json("Not found");
