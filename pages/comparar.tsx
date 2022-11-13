50d96acb959935a25a415c62fd690696a9a19a25
Carlos Cepeda <carlos@cepeda.me>
2022-11-13 14:29:50 +0100

    new compare ux

--- pages/comparar.tsx
+++ pages/comparar.tsx
@@ -7,13 +7,18 @@ import {
   Text,
   VStack,
 } from "@chakra-ui/react";
-import { AsyncSelect, components, OptionProps } from "chakra-react-select";
+import {
+  AsyncSelect,
+  components,
+  OptionProps,
+  SingleValueProps,
+} from "chakra-react-select";
 import Fuse from "fuse.js";
 import { GetStaticProps } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useMemo, useState } from "react";
+import { Fragment, useMemo, useState } from "react";
 import GeneralHead from "../components/head";
 import Main from "../components/layout/main";
 import PageTitle from "../components/layout/page-title";
@@ -93,6 +98,13 @@ const Option = (props: OptionProps<SearchTV>) => {
   );
 };
 
+function SingleValue(props: SingleValueProps<SearchTV>) {
+  const { children, ...rest } = props;
+  const { selectProps } = props;
+  if (selectProps.menuIsOpen) return <Fragment></Fragment>;
+  return <components.SingleValue {...rest}>{children}</components.SingleValue>;
+}
+
 const ComparePage = ({ tvs }: { tvs: SearchTV[] }) => {
   const router = useRouter();
   const fuse = useMemo(() => {
@@ -107,10 +119,10 @@ const ComparePage = ({ tvs }: { tvs: SearchTV[] }) => {
     });
   }, [tvs]);
 
-  const [tv1, setTv1] = useState<SearchTV>(
-    tvs.find((tv) => tv.slug === router.query.tv) || tvs[48]
+  const [tv1, setTv1] = useState<SearchTV | undefined>(
+    tvs.find((tv) => tv.slug === router.query.tv)
   );
-  const [tv2, setTv2] = useState<SearchTV>(tvs[1]);
+  const [tv2, setTv2] = useState<SearchTV>();
 
   const handleSearch = (value: string) => {
     const searched = fuse.search(value, { limit: 10 });
@@ -130,33 +142,40 @@ const ComparePage = ({ tvs }: { tvs: SearchTV[] }) => {
         alignItems="center"
       >
         <VStack flex="1" maxWidth="400px">
-          <SearchTvItem
-            key={tv1.id}
-            slug={tv1.slug || ""}
-            fullName={getFullName(tv1)}
-            picture={getPicture(tv1)}
-            score={tv1.score || 0}
-            brand={getBrand(tv1)}
-            imageTechnology={getImageTechnology(tv1)}
-            model={getModel(tv1)}
-            releaseDate={getReleaseDate(tv1)}
-            resolutionIcon={getResolution(tv1)}
-            screenSize={getScreenSize(tv1)}
-            price={tv1.minPrice || 0}
-          />
+          {tv1 && (
+            <SearchTvItem
+              key={tv1.id}
+              slug={tv1.slug || ""}
+              fullName={getFullName(tv1)}
+              picture={getPicture(tv1)}
+              score={tv1.score || 0}
+              brand={getBrand(tv1)}
+              imageTechnology={getImageTechnology(tv1)}
+              model={getModel(tv1)}
+              releaseDate={getReleaseDate(tv1)}
+              resolutionIcon={getResolution(tv1)}
+              screenSize={getScreenSize(tv1)}
+              price={tv1.minPrice || 0}
+            />
+          )}
           <Box width="100%">
             <AsyncSelect<SearchTV>
               instanceId="tv1"
               name="tv1"
               colorScheme="gray"
-              placeholder="Selecciona una TV"
-              defaultOptions={handleSearch("oled")}
+              placeholder="Busca por modelo o EAN"
+              noOptionsMessage={() => "Sin resultados"}
               loadOptions={(inputValue, callback) => {
                 callback(handleSearch(inputValue));
               }}
+              defaultOptions={handleSearch(
+                tv1?.general?.brand?.serie?.data?.attributes?.name || ""
+              )}
+              openMenuOnFocus={false}
+              openMenuOnClick={false}
               getOptionValue={(opt) => opt?.ean || ""}
               getOptionLabel={(opt) => opt?.general?.brand?.model || ""}
-              components={{ Option }}
+              components={{ SingleValue, Option }}
               value={tv1}
               onChange={(value) => value && setTv1(value)}
             />
@@ -166,39 +185,44 @@ const ComparePage = ({ tvs }: { tvs: SearchTV[] }) => {
           vs
         </Text>
         <VStack flex="1" maxWidth="400px">
-          <SearchTvItem
-            key={tv2.id}
-            slug={tv2.slug || ""}
-            fullName={getFullName(tv2)}
-            picture={getPicture(tv2)}
-            score={tv2.score || 0}
-            brand={getBrand(tv2)}
-            imageTechnology={getImageTechnology(tv2)}
-            model={getModel(tv2)}
-            releaseDate={getReleaseDate(tv2)}
-            resolutionIcon={getResolution(tv2)}
-            screenSize={getScreenSize(tv2)}
-            price={tv2.minPrice || 0}
-          />
+          {tv2 && (
+            <SearchTvItem
+              key={tv2.id}
+              slug={tv2.slug || ""}
+              fullName={getFullName(tv2)}
+              picture={getPicture(tv2)}
+              score={tv2.score || 0}
+              brand={getBrand(tv2)}
+              imageTechnology={getImageTechnology(tv2)}
+              model={getModel(tv2)}
+              releaseDate={getReleaseDate(tv2)}
+              resolutionIcon={getResolution(tv2)}
+              screenSize={getScreenSize(tv2)}
+              price={tv2.minPrice || 0}
+            />
+          )}
           <Box width="100%">
             <AsyncSelect<SearchTV>
               instanceId="tv2"
               name="tv2"
               colorScheme="gray"
-              placeholder="Selecciona una TV"
+              placeholder="Busca por modelo o EAN"
+              noOptionsMessage={() => "Sin resultados"}
+              openMenuOnFocus={false}
+              openMenuOnClick={false}
               loadOptions={(inputValue, callback) => {
                 callback(handleSearch(inputValue));
               }}
               getOptionValue={(opt) => opt?.ean || ""}
               getOptionLabel={(opt) => opt?.general?.brand?.model || ""}
-              components={{ Option }}
+              components={{ SingleValue, Option }}
               value={tv2}
               onChange={(value) => value && setTv2(value)}
             />
           </Box>
         </VStack>
       </VStack>
-      <Link href={`vs/${tv1.slug}-vs-${tv2.slug}`} passHref>
+      <Link href={`vs/${tv1?.slug}-vs-${tv2?.slug}`} passHref>
         <Button
           as="a"
           alignSelf="center"
@@ -206,6 +230,7 @@ const ComparePage = ({ tvs }: { tvs: SearchTV[] }) => {
           backgroundColor="red.700"
           color="white"
           width="300px"
+          disabled={!tv1 || !tv2}
           _hover={{
             backgroundColor: "red.900",
           }}
