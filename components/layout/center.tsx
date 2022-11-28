6b22fded115070d29916624d84d7a8af8cc63c88
Carlos Cepeda <carloscgarc@inditex.com>
2022-11-28 17:13:02 +0100

    set width to center component

--- components/layout/center.tsx
+++ components/layout/center.tsx
@@ -5,6 +5,7 @@ const Center = (props: FlexProps) => {
     <Flex
       maxW={{ lg: "75em" }}
       mx={{ base: 4, md: 8, xl: "auto" }}
+      width={{ xl: "100%" }}
       {...props}
     />
   );
